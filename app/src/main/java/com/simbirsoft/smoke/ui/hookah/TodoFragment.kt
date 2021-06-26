d26bbb28269cafde10c763e90f634ba1df5d009a
Cyber-Tapok <angrybirds280700@gmail.com>
2021-06-26 18:11:54 +0400

    added other layouts

--- app/src/main/java/com/simbirsoft/smoke/ui/hookah/TodoFragment.kt
+++ app/src/main/java/com/simbirsoft/smoke/ui/hookah/TodoFragment.kt
@@ -3,5 +3,5 @@ package com.simbirsoft.smoke.ui.hookah
 import com.simbirsoft.smoke.R
 import com.simbirsoft.smoke.ui.BaseFragment
 
-class TodoFragment : BaseFragment(R.layout.fragment_todo) {
+class TodoFragment : BaseFragment(R.layout.fragment_hookah_detail) {
 }
